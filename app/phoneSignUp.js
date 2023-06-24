7d5acb66873a39857854a27687a4f651d57bef04
jayLapani <jaylapani45@gmaill.com>
2023-06-24 19:08:50 +0530

    Add Firebase

---
+++ app/phoneSignUp.js
@@ -0,0 +1,109 @@
+'use client'
+import firebase from './firebase';
+import firebase1 from 'firebase/app';
+import 'firebase/app';
+import React from 'react';
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+// import { getAuth, signInWithPhoneNumber } from "firebase/auth";
+
+
+// export default function HomePage() {
+export default class page extends React.Component {
+
+    
+    handleChange = (e) => {
+        const { name, value } = e.target
+        this.setState(
+            {
+                [name]: value,
+            }
+        );
+    };
+    configureCaptcha = () => {
+
+        const auth = getAuth();
+        window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+            'size': 'invisible',
+            'callback': (response) => {
+                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                onSignInSubmit();
+                console.log("recaptcha verified");
+            },
+            defaultCountry: "IN"
+        }, auth);
+    };
+    onSubmitOtp = (e) => {
+        e.preventDefault();
+        const code = this.state.otp;
+        try {
+            
+            confirmationResult.confirm(code).then((result) => {
+                // User signed in successfully.
+                const user = result.user;
+                console.log("Otp Verified");
+                alert("Mobile Number is Verified");
+                this.props.onOtpVerification(true);
+                // ...
+            }).catch((error) => {
+                alert("Incorrect OTP Enter Again!!")
+                // User couldn't sign in (bad verification code?)
+                // ...
+            });
+            
+        } catch (error) {
+            alert("Incorrect OTP Enter Again!!")
+        }
+    }
+
+    onSignInSubmit = (e) => {
+        document.getElementById('otpform').style.display = 'flex';
+        e.preventDefault();
+        this.configureCaptcha();
+
+        const  phoneNumber1  = "+91" + this.props.phoneNumber;
+        console.log(phoneNumber1);
+        const appVerifier = window.recaptchaVerifier;
+
+        const auth = getAuth();
+        signInWithPhoneNumber(auth, phoneNumber1, appVerifier)
+            .then((confirmationResult) => {
+                // SMS sent. Prompt user to type the code from the message, then sign the
+                // user in with confirmationResult.confirm(code).
+                window.confirmationResult = confirmationResult;
+                // ...
+            }).catch((error) => {
+                // Error; SMS not sent
+                // ...
+            });
+    };
+    //   const sendVerificationCode = () => {
+    //     const phoneNumber = "+1234567890"; // Enter the phone number you want to verify here
+
+    //     const appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container'); // Replace 'recaptcha-container' with the ID of your reCAPTCHA container element
+
+    //     firebase.auth.signInWithPhoneNumber(phoneNumber,appVerifier)
+    //       .then((confirmationResult) => {
+    //         const verificationId = confirmationResult.verificationId;
+    //         localStorage.setItem('verificationId', verificationId);
+    //         // Proceed with the OTP verification
+    //       })
+    //       .catch((error) => {
+    //         console.error(error);
+    //       });
+    //   }
+    render() {
+        return (
+            <div>
+                <div id='sign-in-button'></div>
+                {/* <form onSubmit={this.onSignInSubmit}>
+                    <input type='number' name='mobile' value={this.props}  placeholder='Enter Mobile Number' onChange={this.handleChange}></input>
+                    <button type='submit'>Submit</button>
+                </form> */}
+                <form id='otpform' onSubmit={this.onSubmitOtp} style={{display:'none'}}>
+                    <input type='number' name='otp' placeholder='Enter otp Number' required onChange={this.handleChange}></input>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
