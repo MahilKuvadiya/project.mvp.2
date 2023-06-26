568cbcf213ca92d2ef62bd6b4efe0df2d7665ac2
Mahil <mahilpatel6858@gmail.com>
2023-06-26 22:05:54 +0530

    page isolation added

--- app/(justPages)/profile/page.jsx
+++ app/(justPages)/profile/page.jsx
@@ -5,12 +5,20 @@ import { useSession } from 'next-auth/react';
 import axios from 'axios';
 import BlogCard from 'app/components/BlogCard';
 import classes from './page.module.css';
-
+import { useRouter } from 'next/navigation';
 export default function Accounts() {
   const [accounts, setAccounts] = useState([]);
   const { data: session, status } = useSession();
   const [userEmail, setUserEmail] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const anotherSession = useSession()
+  const router = useRouter();
+
+  useEffect(() => {
+    if (anotherSession?.status === 'unauthenticated') {
+       router.push('/') 
+    }
+})
 
   useEffect(() => {
     async function fetchAccounts() {
