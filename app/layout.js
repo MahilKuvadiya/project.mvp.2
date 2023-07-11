dedc05a4674ea627990b724d9a918e1f0312b94a
Mahil Kuvadiya <127921738+MahilKuvadiya@users.noreply.github.com>
2023-07-11 17:02:03 +0000

    head tag added

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7128324200205198"
+     crossorigin="anonymous"></script>
+      </head>
       <body>
         <Provider>
           <ToasterContext/>
